188c99f4a4e12c892d0c528495afeb45851b27ee
Remco Mokveld <remco.mokveld@disneystreaming.com>
2020-05-31 22:36:39 +0200

    Add an example of an instrumented test with hilt

---
+++ app/src/androidTest/java/com/deviange/daggerhilt/HiltTestRunner.kt
@@ -0,0 +1,15 @@
+package com.deviange.daggerhilt
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
+
+@Suppress("unused")
+class HiltTestRunner : AndroidJUnitRunner() {
+    override fun newApplication(
+        cl: ClassLoader?,
+        className: String?,
+        context: Context?
+    ): Application = super.newApplication(cl, HiltTestApplication::class.java.name, context)
+}
