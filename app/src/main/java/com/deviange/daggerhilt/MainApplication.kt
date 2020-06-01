e786907d1ae906669a7c2fb8cf876c7bb4152f5e
Ahmed I. Khalil <ahmedibrahimkhali@gmail.com>
2020-06-02 01:07:38 +0200

    Demonstrate hilt with WorkManager

--- app/src/main/java/com/deviange/daggerhilt/MainApplication.kt
+++ app/src/main/java/com/deviange/daggerhilt/MainApplication.kt
@@ -1,6 +1,8 @@
 package com.deviange.daggerhilt
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.EntryPoint
@@ -8,9 +10,17 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.HiltAndroidApp
 import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.components.ApplicationComponent
+import javax.inject.Inject
 
 @HiltAndroidApp
-class MainApplication : Application()
+class MainApplication : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override fun getWorkManagerConfiguration(): Configuration =
+        Configuration.Builder().setWorkerFactory(workerFactory).build()
+}
 
 @EntryPoint
 @InstallIn(ActivityComponent::class)
@@ -22,5 +32,5 @@ interface RepositoryLocator {
 @InstallIn(ApplicationComponent::class)
 object MainAppModule {
     @Provides
-    fun repository() = Repository()
+    fun repository(): Repository = RealRepository()
 }
